ed2a12b378b3a2ba3bf32e60a4424b8b613b834c
Georg Skuggedal <georgskuggedal@Georgs-MacBook-Pro.local>
2019-12-06 18:44:16 +0100

    first changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import bilde from './georgbilde.JPG'
+import ullogo from './ul.jpg'
+import ulapp from './ulapp.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="frontpage-background">
+      <div className='frontpage-projects-wrapper'>
+        <h1>Georg Skuggedal</h1>
+        <h2>Mine prosjekter:</h2>
+        <div className='project'>
+          <img src={ullogo} alt="IMG" className='project-image'/>
+          <div className=''>
+            <h3>UL.no</h3>
+            <p>tekst her</p>
+          </div>
+        </div>
+        <div className='project'>
+          <img src={ulapp} alt="IMG" className='project-image'/>
+          <div className=''>
+            <h3>UL.no</h3>
+            <p>tekst her</p>
+          </div>
+        </div>
+      </div> 
+      <div className='frontpage-sidebar-right'>
+          <img src={bilde} alt="IMG" className='frontpage-bilde'/>
+          <p>Mitt navn er Georg Skuggedal og er en 2. års student ved NTNU på kommunikasjonsteknologi.</p>
+      </div>
+      
     </div>
   );
 }
